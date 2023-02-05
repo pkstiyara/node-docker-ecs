44ab9b1c8468f89d3410f7619b8200a7bc46301c
pkstiyara <pkstiyara@gmail.com>
2023-02-05 10:39:25 +0530

    added the node files

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3300
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Well Done Priyanshu'))
+app.listen(port, () => console.log('Example app listing on port ${port} ! '))
